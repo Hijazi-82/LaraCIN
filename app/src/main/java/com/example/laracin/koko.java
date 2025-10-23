3520c5cb9dd748b198ea6e9ead421a2769b38671
class01 <daudhamza@gmail.com>
2025-10-23 08:47:28 +0300

    ygjytfdf

--- app/src/main/java/com/example/laracin/koko.java
+++ app/src/main/java/com/example/laracin/koko.java
@@ -13,6 +13,7 @@ public class koko extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /// יחםךיעחןלעין
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_koko);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
